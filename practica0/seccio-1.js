f973f45e9e8ea93b87cabeba04fb6367151c045b
ovasquezz <56310550+ovasquezz@users.noreply.github.com>
2020-10-13 20:44:57 +0200

    ex3

--- practica0/seccio-1.js
+++ practica0/seccio-1.js
@@ -15,11 +15,18 @@ function signe() {
 
 //Es número   Escriu un codi que verifiqui si el que introdueix l’usuari en una casella és un nombre
 function nombre() {
-    input1 = parseInt(document.getElementById("input1").value,10);
+    input1 = parseInt(document.getElementById("input1").value, 10);
 
     if (!isNaN(input1)) {
         document.getElementById("results").innerHTML = "Es un número"
     } else {
         document.getElementById("results").innerHTML = "No es un número"
     }
+}
+
+//Número part entera   Escriu un codi que donat un número real, mostri només la part sencera (esborri els decimals).
+function entero(){
+    input1 = document.getElementById("input1").value;
+
+    document.getElementById("results").innerHTML = Math.trunc(input1);
 }
